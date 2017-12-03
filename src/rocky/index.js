844733cf1913b03714631469bba59256bdd6cd7d
Alexandra <AADeLucia@users.noreply.github.com>
2017-12-02 22:17:22 -0500

    fixed UI
    added time at top and corrected text alignment

--- src/rocky/index.js
+++ src/rocky/index.js
@@ -4,12 +4,36 @@ var rocky = require('rocky');
 // Global object to store fact string
 var factString;
 
+/*
+function drawFibb(ctx, screenHeight, screenWidth){
+	var newFib = 1 * 15;
+	
+	ctx.strokeStyle = 'blue';
+	
+	// rect 1
+	ctx.strokeRect(screenWidth/2, screenHeight/2, newFib, newFib);
+	val oldFib = 1;
+	
+	// rect 2
+	ctx.strokeRect(screenWidth/2 - newFib, screenHeight/2, newFib, newFib);
+	
+	// rect 3
+	fibbVal = 2 * 15;
+	ctx.strokeRect(screenWidth/2 - fibbVal)
+	
+	return;
+}
+*/
+
 rocky.on('minutechange', function(event) {
   // get string parameter
   var d = new Date();
-  var hour = d.getHours();
-  var minute = d.getMinutes();
-  var number = hour.toString() + minute.toString();
+  var hour = d.getHours().toString();
+  var minute = d.getMinutes().toString();
+	if(minute.length == 1){
+		minute = "0" + minute;
+	}
+  var number = hour + minute;
   // Send a message to obtain math fact
   rocky.postMessage({'fetch': true, 'number': number});
 });
@@ -29,23 +53,36 @@ rocky.on('message', function(event) {
 
 rocky.on('draw', function(event) {
   var ctx = event.context;
-  var d = new Date();
-
   // Clear the screen
-  ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
-  ctx.fillText(d.getHours() + ":" + d.getMinutes(), ctx.canvas.unobstructedWidth / 2, 2);
+	ctx.fillStyle = 'blue';
+	var width = ctx.canvas.clientWidth;
+	var height =  ctx.canvas.clientHeight;
+  ctx.clearRect(0, 0, width, height);
 
-  // Draw the conditions (before clock hands, so it's drawn underneath them)
+	// Draw fibb spiral
+	//drawFibb(ctx, height, width);
+	
+	// Draw time
+	var d = new Date();
+  var hour = d.getHours().toString();
+  var minute = d.getMinutes().toString();
+	if(minute.length == 1){
+		minute = "0" + minute;
+	}
+	
+	var dateString = hour + ":" + minute;
+	ctx.fillStyle = 'white';
+	ctx.textAlign = 'center';
+	ctx.font = '32px bold numbers Leco-numbers';
+  ctx.fillText(dateString, width/2, 0, width);
+
+  // Draw fact
   if (factString) {
       // Draw the text, top center
-      ctx.fillStyle = 'lightgray';
+      ctx.fillStyle = 'white';
       ctx.textAlign = 'center';
-      ctx.font = '14px Gothic';
-    
-      
-     var formattedString = factString.substring(0,factString.length/2) + '\n'
-                                                + factString.substring(factString.length/2 + 1, factString.length);
-      ctx.fillText(factString, 80, 0, 144);
-      //ctx.fillText(formattedString, ctx.canvas.unobstructedWidth / 2, 4);
+      ctx.font = '18px Gothic';
+		
+      ctx.fillText(factString, width/2, height*(1/3), width); // text is centered around the x-coordinate
   }
 });
