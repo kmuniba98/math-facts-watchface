80a0dcb929b1f69ba756a44b23b1d4629367df21
Muniba Khan <mkhan@rollins.edu>
2017-12-03 10:25:02 -0500

    Added default.

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,20 +1,49 @@
 // PebbleKit JS (pkjs)
 
+// Makes API call for a math fact
+function getMathFact(number) {
+	var url = "http://numbersapi.com/" + number + "/math?default=not+found";
+	var fact = 'I am here!';
+	request(url, 'GET', function(respText) {
+		//return 'I am here!';
+		//return respText;
+		fact = respText;
+	});
+	return fact;
+}
+
+// Makes API call for a trivia fact
+function getTriviaFact(number) {
+	var url = "http://numbersapi.com/" + number + "/trivia?default=not+found";
+	request(url, 'GET', function(respText) {
+		return respText;
+	});
+}
+
 Pebble.on('message', function(event) {
   // Get the message that was passed
   var message = event.data;
   
-  // Request math facts
+  // Request fact based on the time
   if (message.fetch) {
-      var url = "http://numbersapi.com/" + message.number + "/math";
-    
-        request(url, 'GET', function(respText) {
-          var factReply = respText;
-  
-          Pebble.postMessage({
-            'factString': factReply
-          });
-        });
+		// if math fact is not found, use trivia fact
+		var url = "http://numbersapi.com/" + message.number + "/math?default=not+found";
+		request(url, 'GET', function(respText) {
+			var factReply = respText;
+			if (respText == 'not found') {
+				url = "http://numbersapi.com/" + message.number + "/trivia";
+				request(url, 'GET', function(respText) {
+								var factReply = respText;
+								Pebble.postMessage({
+								'factString': factReply
+								});
+			});
+		}
+					
+			Pebble.postMessage({
+				'factString': factReply
+			});
+		});
   }
 });
 
